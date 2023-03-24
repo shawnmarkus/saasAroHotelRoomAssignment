2a18cfe6a06e09604d3f2a42300d3ab2f8b597f3
shawnmarkus <shawnmarkus4200@gmail.com>
2023-03-24 17:26:02 +0530

    change the end point

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Premium from "./Component/Premium";
-import Standard from "./Component/Standard";
 import Home from "./Component/Home";
 import NavbarComp from "./Component/NavBarComp";
 import "./global.css";
+import Room from "./Component/Room";
 
 function App() {
   return (
@@ -13,9 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          {/* <Route exact path="/room" element={<Room />} /> */}
-          {/* <Route exact path="/standard" element={<Premium />} /> */}
-          {/* <Route exact path="/premium" element={<Standard />} /> */}
+          <Route exact path="/room" element={<Room />} />
         </Routes>
       </BrowserRouter>
     </div>
