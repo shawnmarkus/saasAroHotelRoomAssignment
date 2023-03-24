e0fea5ec3a457288121f3359fec57a67a592cbca
shawnmarkus <shawnmarkus4200@gmail.com>
2023-03-24 17:27:14 +0530

    created grid of premium room list

--- src/Component/Premium.jsx
+++ src/Component/Premium.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import dataSet from "../Data/data.json";
+import CardComp from "./CardComp";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { Container } from "react-bootstrap";
 
 const Premium = () => {
-  return <div>Premium</div>;
+  return (
+    <div className="p-2 m-2">
+      <Container>
+        <Row xs={1} md={2} className="g-4">
+          {dataSet.map((data, idx) => {
+            if (data.hotelType === "premium") {
+              return (
+                <Col key={idx}>
+                  <CardComp data={data} />
+                </Col>
+              );
+            }
+            return null;
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
+// {
+/* <CardComp key={idx} data={data} /> */
+// }
+
+//        {dataSet.map((data, idx) => {
+//   if (data.hotelType === "premium") {
+//     return (
+
+//     );
+//   }
+//   return null;
+// })}
 export default Premium;
