f378d82365a9d02b8e55ef146002fe8f0b946bca
shawnmarkus <shawnmarkus4200@gmail.com>
2023-03-25 13:31:03 +0530

    added cart icon

--- src/Component/NavBarComp.jsx
+++ src/Component/NavBarComp.jsx
@@ -1,16 +1,27 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import Nav from "react-bootstrap/Nav";
 import style from "../css/NavBarComp.module.css";
+import cart from "../assets/svgs/cart.svg";
+import { useNavigate } from "react-router-dom";
 
 function ResponsiveNavbar() {
+  const navigate = useNavigate();
   //   const icons = [HomeIcon, Binoculars, SettingIcon, BellIcon];
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Container>
+        <Container className="d-flex flex-row justify-content-between">
           <Navbar.Brand href="#home" className={style.navbarTitle}>
             <b>Hotel booking</b>
           </Navbar.Brand>
+
+          <Nav>
+            <div onClick={() => navigate("/cart")}>
+              <Image src={cart}></Image>
+            </div>
+          </Nav>
         </Container>
       </Navbar>
     </>
