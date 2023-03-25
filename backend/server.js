dbbcd9780820126c841dbcf5bb8a7b2817a01b91
shawnmarkus <shawnmarkus4200@gmail.com>
2023-03-25 23:42:20 +0530

    changed the orgin

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://frontendofhotelbooking.onrender.com",
     credentials: true,
     methods: ["PUT", "GET", "POST", "PATCH", "DELETE"],
   })
